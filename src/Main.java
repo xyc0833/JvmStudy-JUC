f85ef2da70f8a97ee0726be8ca5ad9bfdb97c902
xyc0833 <2031362123@qq.com>
2026-03-05 15:54:07 +0800

    原子类

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,9 @@
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        //构造一个非公平锁
-        ReentrantLock lock = new ReentrantLock(true);
-        Runnable action = () -> {
-            System.out.println("线程" + Thread.currentThread().getName() + " 开始获取锁...");
-            lock.lock();
-            System.out.println("线程 "+Thread.currentThread().getName()+" 成功获取锁！");
-            lock.unlock();
-        };
-        //等价于
-//        Runnable action = new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("线程" + Thread.currentThread().getName() + " 开始获取锁...");
-//                lock.lock();
-//                System.out.println("线程 "+Thread.currentThread().getName()+" 成功获取锁！");
-//                lock.unlock();
-//            }
-//        };
-        for (int i = 0; i < 10; i++) {   //建立10个线程
-            new Thread(action, "T"+i).start();
-        }
+    public static void main(String[] args) {
+        AtomicInteger integer = new AtomicInteger(1);
+        System.out.println(integer.compareAndSet(1,2));
+        System.out.println(integer.compareAndSet(3,2));
     }
 }
